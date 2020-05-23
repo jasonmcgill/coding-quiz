71d1a1529ba84ef4cae0258415d27b925a55fe48
jasonmcgill <64054045+jasonmcgill@users.noreply.github.com>
2020-05-22 20:38:07 -0600

    psuedo code

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+//timer function
+
+//displayBeginning function
+
+//displayNewQuestion function
+
+//displayAfterAnswer function
+
+//displayHighScoreEntry function
+
+//displayHighScoreList function
+
+//startGame function
+
+//chooseAnswer function
+
+//highScoreSubmit function
+
+//clearHighScores
