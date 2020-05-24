d795e90353dafd4ff555177f7ef430a206c79344
jasonmcgill <64054045+jasonmcgill@users.noreply.github.com>
2020-05-23 21:51:03 -0600

    finished startGame

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,32 +2,39 @@ var timerEl = document.getElementById("time");
 var bodyEl = document.getElementById("main");
 var waitingForAnswer = false;
 var timeLeft = 76;
+   
+    
 
 //object for questions
-    //question string
-    //answer string array
-    //correct index of answer string array
 var questionArray = [
     {
+        //question string
         question: "Commonly used data types DO NOT include:",
-        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"]
+        //answer string array
+        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        //correct index of answer string array  
+        correct: 2
     },
     {
         question: "The condition in an if / else statement is enclosed with _____.",
-        answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"]
+        answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        correct: 2
     },
     {
         question: "Array in JavaScript can be used to store",
-        answers: ["1 numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"]
+        answers: ["1 numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correct: 3
     },
     {
         question: "String values must be enclosed within ______ when being assigned to variables",
-        answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"]
+        answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        correct: 2
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"]
-    },
+        answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correct: 3
+    }
 
 ]
 
@@ -78,13 +85,18 @@ var startGame = function() {
     timer();
     //for loop that runs through question array.length
     for (var i=0; i < questionArray.length; i++){
+         //call displayNewQuestion(question array[i])
+         displayNewQuestion(questionArray[i]);
+        //set waitingForAnswer to be true
+         waitingForAnswer = true;
+         //wait for waitingForAnswer to be false in while loop
+         while (waitingForAnswer) {
 
-    }
+         }
     
-    //call displayNewQuestion(question array[i])
-    //set waitingForAnswer to be true
-    //wait for waitingForAnswer to be false in while loop
+    }   
     //when for loop is finished calls endGame()
+    endGame()
 };
     
 
